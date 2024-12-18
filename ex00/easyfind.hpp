be683f87869076e28d970d82176b03560ccc587d
Nick Talmon <ntalmon@3-G-2.42heilbronn.de>
2024-12-18 16:21:31 +0100

    correction ex00

--- ex00/easyfind.hpp
+++ ex00/easyfind.hpp
@@ -6,7 +6,7 @@
 /*   By: ntalmon <ntalmon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/18 15:47:25 by ntalmon           #+#    #+#             */
-/*   Updated: 2024/12/18 15:55:01 by ntalmon          ###   ########.fr       */
+/*   Updated: 2024/12/18 16:19:06 by ntalmon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,10 @@
 #include <algorithm>
 
 template <typename T>
-int easyfind(T &container, int value)
+typename T::iterator easyfind(T &container, int value)
 {
 	typename T::iterator it = std::find(container.begin(), container.end(), value);
 	if (it == container.end())
 		throw std::exception();
-	return *it;
+	return it;
 }
--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ntalmon <ntalmon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/18 15:37:46 by ntalmon           #+#    #+#             */
-/*   Updated: 2024/12/18 16:04:37 by ntalmon          ###   ########.fr       */
+/*   Updated: 2024/12/18 16:17:56 by ntalmon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,22 +20,22 @@ int main()
 	try
 	{
 		std::cout << "[TEST] Looking for value 5 in the vector." << std::endl;
-		std::cout << "[RESULT] Found value: " << easyfind(vec, 5) << std::endl;
+		std::cout << "[RESULT] Found value: " << *easyfind(vec, 5) << std::endl;
 
 		std::cout << "[TEST] Looking for value 10 in the vector." << std::endl;
-		std::cout << "[RESULT] Found value: " << easyfind(vec, 10) << std::endl;
+		std::cout << "[RESULT] Found value: " << *easyfind(vec, 10) << std::endl;
 
 		std::cout << "[TEST] Looking for value 42 in the vector." << std::endl;
-		std::cout << "[RESULT] Found value: " << easyfind(vec, 42) << std::endl;
+		std::cout << "[RESULT] Found value: " << *easyfind(vec, 42) << std::endl;
 
 		std::cout << "[TEST] Looking for value 1 in the vector." << std::endl;
-		std::cout << "[RESULT] Found value: " << easyfind(vec, 1) << std::endl;
+		std::cout << "[RESULT] Found value: " << *easyfind(vec, 1) << std::endl;
 
 		std::cout << "[TEST] Looking for value 7 in the vector." << std::endl;
-		std::cout << "[RESULT] Found value: " << easyfind(vec, 7) << std::endl;
+		std::cout << "[RESULT] Found value: " << *easyfind(vec, 7) << std::endl;
 
 		std::cout << "[TEST] Looking for value 0 in the vector." << std::endl;
-		std::cout << "[RESULT] Found value: " << easyfind(vec, 0) << std::endl;
+		std::cout << "[RESULT] Found value: " << *easyfind(vec, 0) << std::endl;
 	}
 	catch (const std::exception &e)
 	{
